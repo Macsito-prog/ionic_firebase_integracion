c8a55be8044e5bf5dbdea34c51a157129b90c14c
recxar <it.gariazzo@duocuc.cl>
2022-10-23 17:29:51 -0300

    commit jeje

--- src/app/tarjetas/youtube/youtube.page.ts
+++ src/app/tarjetas/youtube/youtube.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Plugins } from '@capacitor/core';
-import * as WebVPPlugin from 'capacitor-video-player';
-const { CapacitorVideoPlayer,Device } = Plugins;
+import { DomSanitizer,SafeResourceUrl } from '@angular/platform-browser';
 
 
 @Component({
@@ -10,31 +8,17 @@ const { CapacitorVideoPlayer,Device } = Plugins;
   styleUrls: ['./youtube.page.scss'],
 })
 export class YoutubePage implements OnInit {
-  private _videoPlayer:any;
-  private _url: string;
+
+ 
   constructor() { }
 
 
 
   
-  async ngOnInit() {
-    // define the plugin to use
-    const info = await Device.getInfo();
-    if (info.platform === "ios" || info.platform === "android") {
-      this._videoPlayer = CapacitorVideoPlayer;
-    } else {
-      this._videoPlayer = WebVPPlugin.CapacitorVideoPlayer
-    }
-    // define the video url
-    this._url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuck/Bunny.mp4"
-    // add listeners to the plugin
+  ngOnInit() {
+
    
   }
 
-  play(){
-  
-    const res:any  =  this._videoPlayer.initPlayer({mode:"fullscreen",url:this._url,playerId:"fullscreen",componentTag:"my-page"});
-   
   }
-}
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
   firebase: {
-    projectId: 'appfirebase001d',
-    appId: '1:649049872434:web:bd0ed7f09a1c2fafd7842b',
-    storageBucket: 'appfirebase001d.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyD8mn3YjlTMlBbrVka8KGx73J0Pta8_jTE',
-    authDomain: 'appfirebase001d.firebaseapp.com',
-    messagingSenderId: '649049872434',
+    apiKey: "AIzaSyCSxrJ_R0AYNkZnDwBytAWLbDaz_wjVbAI",
+    authDomain: "prueba-2-4f7fa.firebaseapp.com",
+    projectId: "prueba-2-4f7fa",
+    storageBucket: "prueba-2-4f7fa.appspot.com",
+    messagingSenderId: "472766860167",
+    appId: "1:472766860167:web:07e497b5321c4edea4e122"
   },
   production: true
 };
